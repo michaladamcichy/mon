44d4b44592341f87c970c276c10657536492812f
Michał Cichy <mcichy@insert-ad.insert.intranet>
2022-06-22 15:49:20 +0200

    backend priorities

--- algorithm/MapObject.cs
+++ algorithm/MapObject.cs
@@ -233,7 +233,7 @@ namespace algorithm
     public class UnitJSON //alert todo dać dziedziczenie może będzie działać
     {
         public Position position {  get; set; } = new Position();
-
+        public int priority;
         public UnitJSON()
         {
 
@@ -247,9 +247,10 @@ namespace algorithm
 
     public class Unit : MapObject
     {
+        public int priority = 1;
         public Unit(UnitJSON unitJSON) : base(unitJSON.position)
         {
-
+            this.priority = unitJSON.priority;
         }
         public Unit() { }
 
